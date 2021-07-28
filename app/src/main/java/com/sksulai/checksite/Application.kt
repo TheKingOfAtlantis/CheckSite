f46da9b4875a4fb23d9cde3d3b8382e38f6c9656
Sulaiman Sulaiman <SKSulai@gmail.com>
2021-07-28 20:23:45 +0100

    Added Hilt and Room
    Additionally updated minSdk to 26 so I can use java.time.*

---
+++ app/src/main/java/com/sksulai/checksite/Application.kt
@@ -0,0 +1,6 @@
+package com.sksulai.checksite
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp class CheckSiteApp : Application()
--- app/src/main/java/com/sksulai/checksite/MainActivity.kt
+++ app/src/main/java/com/sksulai/checksite/MainActivity.kt
@@ -9,8 +9,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.sksulai.checksite.ui.theme.CheckSiteTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
