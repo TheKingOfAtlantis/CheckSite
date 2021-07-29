c87ac4d525f3683ee87e6d8161f30cc48419d9bb
Sulaiman Sulaiman <SKSulai@gmail.com>
2021-07-29 03:44:13 +0100

    Worker now can download page
    Added: Method to download the webpage
    Updated: Worker now CoroutineWorker

--- app/src/main/java/com/sksulai/checksite/workers/CheckSiteWorker.kt
+++ app/src/main/java/com/sksulai/checksite/workers/CheckSiteWorker.kt
@@ -2,16 +2,30 @@ package com.sksulai.checksite.workers
 
 import android.content.Context
 import androidx.hilt.work.HiltWorker
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import java.io.IOException
+import java.net.URL
 
 @HiltWorker class CheckSiteWorker @AssistedInject constructor(
     @Assisted context: Context,
-    @Assisted params: WorkerParameters
-) : Worker(context, params) {
-    override fun doWork(): Result {
-        TODO("Not yet implemented")
+    @Assisted params: WorkerParameters,
+    val repo: WorkerRepo
+) : CoroutineWorker(context, params) {
+    private fun fromUrl(url: Uri): ByteArray {
+        return try {
+            val inputStream = URL(url.toString()).openStream()
+            inputStream.readBytes().also {
+                inputStream.close()
+            }
+        } catch(e: IOException) {
+            e.printStackTrace()
+            byteArrayOf()
+        }
+    }
+
+    override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
     }
 }
