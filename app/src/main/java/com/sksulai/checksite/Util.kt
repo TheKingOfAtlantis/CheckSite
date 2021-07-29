f0bf2dde45793c47632749805ff9768f3b778884
Sulaiman Sulaiman <SKSulai@gmail.com>
2021-07-29 03:51:28 +0100

    Fixed: Missing extension for Bundle
    Forgot to add this earlier

---
+++ app/src/main/java/com/sksulai/checksite/Util.kt
@@ -0,0 +1,11 @@
+package com.sksulai.checksite
+
+import android.os.Bundle
+import kotlin.reflect.KProperty
+
+@Suppress(
+    "NOTHING_TO_INLINE",
+    "UNCHECKED_CAST"
+) inline operator fun <T> Bundle.getValue(
+    thisObj: Any?, property: KProperty<*>
+): T = get(property.name) as T
